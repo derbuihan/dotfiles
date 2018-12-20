72b5a39d3a37ba82e88130cc314053e2f2d891e6
derbuihan <derbuihan@gmail.com>
2018-12-20 21:51:58 +0900

    add coq config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,7 +15,6 @@
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
-
 (setq default-buffer-file-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (prefer-coding-system 'utf-8)
@@ -65,12 +64,21 @@
 ;; ダークテーマにする
 (load-theme 'manoj-dark t)
 
+;; コピペを出来るようにする．
+(cond (window-system (setq x-select-enable-clipboard t)))
+
+;; ProofGeneralの設定
+(setq proof-splash-enable nil)
+
+;; Load company-coq when opening Coq files
+(add-hook 'coq-mode-hook #'company-coq-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (proof-general))))
+ '(package-selected-packages (quote (company-coq company proof-general))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
