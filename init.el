9dec8bc6ed4fc6691a92af981c0ca9716e5413e4
derbuihan <derbuihan@gmail.com>
2022-10-23 04:48:03 +0900

    update .emacs/init.el

---
+++ init.el
@@ -0,0 +1,97 @@
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (add-to-list 'package-archives
+               (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+(package-initialize)
+
+;; 環境を日本語、UTF-8にする
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; スタートアップメッセージを表示させない
+(setq inhibit-startup-message t)
+
+;; バックアップファイルを作成させない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを削除する
+(setq delete-auto-save-files t)
+
+;; タブにスペースを使用する
+(setq-default tab-width 2 indent-tabs-mode nil)
+
+;; 複数ウィンドウを禁止する
+(setq ns-pop-up-frames nil)
+
+;; メニューバーを消す
+(menu-bar-mode -1)
+
+;; ツールバーを消す
+;; (tool-bar-mode -1)
+
+;; 列数を表示する
+(column-number-mode t)
+
+;; 行数を表示する
+(global-linum-mode t)
+
+;; カーソルの点滅をやめる
+(blink-cursor-mode 0)
+
+;; カーソル行をハイライトする
+(global-hl-line-mode t)
+
+;; 対応する括弧を光らせる
+(show-paren-mode 1)
+
+;; 括弧の補完
+(setq skeleton-pair 1)
+(electric-pair-mode 1)
+
+;; スクロールは１行ごとに
+(setq scroll-conservatively 1)
+
+;; "yes or no" の選択を "y or n" にする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; ダークテーマにする
+(load-theme 'manoj-dark t)
+
+;; コピペを出来るようにする．
+(cond (window-system (setq x-select-enable-clipboard t)))
+
+;; tab で補間
+(setq tab-always-indent 'complete)
+
+;; ProofGeneralの設定
+(setq proof-splash-enable nil)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(coq-prog-name "/opt/homebrew/bin/coqtop")
+  '(package-selected-packages
+     '(company-coq company proof-general))
+  '(proof-three-window-enable t))
+
+;; Load company-coq when opening Coq files
+(add-hook 'coq-mode-hook #'company-coq-mode)
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  )
+
